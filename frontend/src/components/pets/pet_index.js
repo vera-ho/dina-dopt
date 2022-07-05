39f286d26a99e45ebc6d91e2aab5ccff98a9d510
Vera Ho <vh71886@gmail.com>
2022-07-05 16:24:39 -0400

    Add CSS to pets index

--- frontend/src/components/pets/pet_index.js
+++ frontend/src/components/pets/pet_index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { receiveAllPets } from "../../actions/pet_actions";
 import { getPets } from "../../util/pet_util";
+import { Link } from "react-router-dom";
 
 const PetsIndex = props => {
     const dispatch = useDispatch();
@@ -18,18 +19,20 @@ const PetsIndex = props => {
 
     if(!pets) return null;
 
-    const petItems = pets.map( pet => (
-        <li className="pet-index-item">
-            <img src="" alt={pet.name} />
-            <p>Pet Name: {pet.name}</p>
-            <p>Pet Type: {pet.petType}</p>
+    const petItems = pets.map( (pet, idx) => (
+        <li className="pet-index-item" key={idx}>
+            <Link to={`/pets/${pet._id}`} className="pet-index-item-link">
+                <img src="" alt={pet.name} className="pet-index-item-img" />
+                <p>Pet Name: {pet.name}</p>
+                <p>Pet Type: {pet.petType}</p>
+            </Link>
         </li>
     ));
 
     return (
         <div className="pets-index-container">
             <div className="pets-index-content">
-                <h1>Dinos Available</h1>
+                {/* <h1>Dinos Available</h1> */}
                 <ul>
                     {petItems}
                 </ul>
