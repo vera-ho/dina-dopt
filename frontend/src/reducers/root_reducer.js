f5e4de55cee5db50f5bca3ae6c16338d2f46c7ae
Kevin <NateraKevin1@gmail.com>
2022-07-05 15:05:04 -0400

    everything works

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors/errors_reducer';
 import sessionAPI from './session_api_reducer';
+import entities from './entities_reducer'
 
 const RootReducer = combineReducers({
+  entities,
   session,
   errors,
   sessionAPI
