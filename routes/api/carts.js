b5a81dbd4976ca03b40814d67256cc5982eafffb
Michael Chen <michael.chen.1120@gmail.com>
2022-07-05 12:14:16 -0400

    Add pets routes

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -2,3 +2,4 @@ const express = require('express');
 const router = express.Router();
 const Cart = require('../../models/Cart');
 
+module.exports = router;
--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const Pet = require('../../models/Pet');
+
+router.get('/', async (req, res) => {
+  const pets = await Pet.find();
+  res.json(pets);
+});
+
+router.post('/', async (req, res) => {
+  const { name, petType, description, price } = req.body;
+  const newPet = await Pet.create({
+    name,
+    petType,
+    description,
+    price,
+  });
+  res.json(newPet);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const pet = await Pet.findById(id);
+  res.json(pet);
+});
+
+module.exports = router;
