8609c55659dbf00e9f6132ecd57108e2d7cc64bb
Vera Ho <vh71886@gmail.com>
2022-07-05 11:33:48 -0400

    Update root reducer for pets reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
 import sessionAPI from './session_api_reducer';
+import petsReducer from './pets_reducer';
 
 const RootReducer = combineReducers({
   session,
   errors,
-  sessionAPI
+  sessionAPI, 
+  petsReducer
 });
 
 export default RootReducer;
