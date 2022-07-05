2e59562f288b502224b2e1ececd83411615fb89b
alancln <alan.ng8912@gmail.com>
2022-07-05 11:37:55 -0400

    Style navigation header. Add google fonts to index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
+import './assets/stylesheets/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
