42acc2f02e3a46527bc2a7367a884f5e78975dda
Vera Ho <vh71886@gmail.com>
2022-07-05 17:28:59 -0400

    Update reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors/errors_reducer';
-import sessionAPI from './session_api_reducer';
 import uiReducer from './ui_reducer';
 import entities from './entities_reducer'
+import petsReducer from './pets_reducer';
 
 const RootReducer = combineReducers({
   entities,
   session,
   errors,
-  sessionAPI,
-  ui: uiReducer
+  ui: uiReducer,
+  pets: petsReducer
 });
 
 export default RootReducer;
