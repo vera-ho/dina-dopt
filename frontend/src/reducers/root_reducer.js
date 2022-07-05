bc41895d7a5cf685b099544ddfc558efc64e9a13
Kevin <NateraKevin1@gmail.com>
2022-07-05 14:55:16 -0400

    fixed imports

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import errors from './errors_reducer';
+import errors from './errors/errors_reducer';
 import sessionAPI from './session_api_reducer';
 
 const RootReducer = combineReducers({
