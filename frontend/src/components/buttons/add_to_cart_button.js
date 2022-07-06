10fcbbacdf7a3b27f1baa7a67520a3b9244d2d6f
Michael Chen <michael.chen.1120@gmail.com>
2022-07-06 14:00:49 -0400

    Add add to cart button

---
+++ frontend/src/components/buttons/add_to_cart_button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createCart, addToCart } from '../../util/cart_api_util';
+
+const AddToCartButton = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let newCart = await addToCart(props.currentCart.id);
+    return dispatch(createCart(newCart.data));
+  };
+
+  return (
+    <div className='add-to-cart-button'>
+      <button onClick={handleSubmit}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default AddToCartButton;
