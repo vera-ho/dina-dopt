5185ae24d5e652f15942cdb5606773c73d45d216
Michael Chen <michael.chen.1120@gmail.com>
2022-07-06 13:12:46 -0400

    Add quantity logic to cart

--- routes/api/carts.js
+++ routes/api/carts.js
@@ -28,16 +28,35 @@ router.post(
     const user = req.user.id;
     const cart = await Cart.find({ userId: user.id });
     let pet = Pet.findById(petId);
-    let petDetails = {
-      petId: petId,
-      quantity: Number.parseInt(req.body.quantity),
-      price: pet.price,
-      total: (cart[0].total += pet.price),
-    };
-    cart[0].items.push(petDetails);
-    res.json(cart[0]);
+    const petIndex = cart.items.findIndex((item) => item.petId === pet.id);
+    if (petIndex === -1) {
+      cart.items[petIndex].quantity =
+        cart.items[petIndex].quantity + req.body.quantity;
+      cart.items[petIndex].total = cart.items[petIndex].quantity * pet.price;
+      cart.items[petIndex].price = pet.price;
+      cart.subTotal = cart.items
+        .map((item) => item.total)
+        .reduce((a, b) => a + b);
+    } else {
+      let petDetails = {
+        petId: petId,
+        quantity: Number.parseInt(req.body.quantity),
+        price: pet.price,
+        total: parseInt(pet.price) * parseInt(req.body.quantity),
+      };
+      cart[0].items.push(petDetails);
+      res.json(cart[0]);
+    }
   }
 );
+
+router.delete('/', async (req, res) => {
+  const { id } = req.params;
+  const cart = await Cart.find({ userId: user.id });
+  let pet = Pet.findById(req.body.petId);
+  cart.items.filter((item) => item.petId !== pet.id);
+  res.json(cart);
+});
 
 // router.post('/user/user_id', async (req, res) => {
 //   const { petId } = req.body;
