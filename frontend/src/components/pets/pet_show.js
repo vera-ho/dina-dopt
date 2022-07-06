a0fb5900cdb844593fceffaac8777dfb0ff870e0
Vera Ho <vh71886@gmail.com>
2022-07-06 11:50:02 -0400

    Update pet show container to map single pet to props

--- frontend/src/components/pets/pet_show.js
+++ frontend/src/components/pets/pet_show.js
@@ -3,18 +3,11 @@ import { useDispatch } from "react-redux";
 import { receiveSinglePet } from "../../actions/pet_actions";
 import { getPet } from "../../util/pet_util";
 import { useLocation } from 'react-router-dom';
-    
 
-    
-
-
-
-const PetsShow = props => {
+const PetShow = props => {
     const dispatch = useDispatch();
-    const { pet } = props;
     const location = useLocation();
-
-
+    const { pet } = props;
 
     useEffect( () => {
         fetchPet();
@@ -22,22 +15,27 @@ const PetsShow = props => {
 
     const fetchPet = async () => {
         let pet = await getPet(location.pathname.replace("/pets/",""));
+        console.log("pet: " + pet);
         dispatch(receiveSinglePet(pet));
     }
 
-    if(!pet || !pet[0]) {
+    if(!pet) {
         return null
     } else { 
 
+    console.log("pet")
+    console.log(pet);
     return (
         <div className="pets-show-container">
             <div className="pets-show-content">
-                <h1>Dino Available</h1>
+                {/* <h1>Dino Available</h1> */}
                 <ul>
                     <li className="pet-show-item">
-                    <img src="" alt={pet[0].name} />
-                     <p>Pet Name: {pet[0].name}</p>
-                     <p>Pet Type: {pet[0].petType}</p>
+                    <img src="https://dina-dopt-seed.s3.amazonaws.com/compressed-full+2/bambiraptor-alex-shaffer-full.jpg" 
+                        alt={pet.name} className="pet-index-item-img" />
+                     <p>Pet Name: {pet.name}</p>
+                     <p>Pet Type: {pet.petType}</p>
+                     <p>Description: {pet.description}</p>
                     </li>
                 </ul>
             </div>
@@ -46,4 +44,4 @@ const PetsShow = props => {
 }
 }
 
-export default PetsShow;
+export default PetShow;
--- frontend/src/components/pets/pet_show_container.js
+++ frontend/src/components/pets/pet_show_container.js
@@ -1,11 +1,24 @@
 import { connect } from "react-redux";
 import { requestSinglePet } from "../../actions/pet_actions";
-import PetsShow from "./pet_show";
+import PetShow from "./pet_show";
 
-const mSTP = state => {
+const mSTP = (state, ownProps) => {
+    let pets = Object.values(state.entities.pets);
+    let petIdx = -1;
+
+    pets.forEach( (pet, idx) => {
+        if(pet._id === ownProps.match.params.pet_id) {
+            petIdx = idx;
+        }
+    });
+
+    console.log(pets[petIdx]);
     return {
-        pet: Object.values(state.entities.pets)
+        pet: pets[petIdx]
     }
+    // return {
+    //     pet: Object.values(state.entities.pets)
+    // }
 }
 
 const mDTP = dispatch => {
@@ -14,4 +27,4 @@ const mDTP = dispatch => {
     }
 }
 
-export default connect(mSTP, mDTP)(PetsShow);
+export default connect(mSTP, mDTP)(PetShow);
