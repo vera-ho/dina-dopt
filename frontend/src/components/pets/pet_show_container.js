45efc3728700f3bc044968eaeb4f7d166b06a5f7
Kevin <NateraKevin1@gmail.com>
2022-07-07 09:56:34 -0400

    checkpoint

--- frontend/src/components/pets/pet_show_container.js
+++ frontend/src/components/pets/pet_show_container.js
@@ -1,16 +1,19 @@
 import { connect } from "react-redux";
 import { requestSinglePet } from "../../actions/pet_actions";
+import { fetchAllReviewsForPet } from "../../actions/review_actions";
 import PetsShow from "./pet_show";
 
 const mSTP = state => {
     return {
-        pet: Object.values(state.entities.pets)
+        pet: Object.values(state.entities.pets),
+        reviews: Object.values(state.entities.reviews)
     }
 }
 
 const mDTP = dispatch => {
     return {
         requestSinglePet: (petId) => dispatch(requestSinglePet(petId)),
+        fetchAllReviewsForPet: (petId) => dispatch(fetchAllReviewsForPet(petId)),
     }
 }
 
