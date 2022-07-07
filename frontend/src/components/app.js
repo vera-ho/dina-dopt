9cd3818585115c7c6c1ddd2bfba9fdbe8eeeebf2
Michael Chen <michael.chen.1120@gmail.com>
2022-07-07 12:15:10 -0400

    Add cart show component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,6 +7,7 @@ import SignupFormContainer from './session/signup_form_container';
 import PetsContainer from './pets/pet_container';
 import PetsShowContainer from './pets/pet_show_container';
 import SplashPage from './splash/splash_page';
+import Cart from './cart/cart';
 
 const App = () => (
     <div className="app-content">
@@ -15,6 +16,7 @@ const App = () => (
         <Switch>
             <ProtectedRoute path="/pets/:pet_id" component={PetsShowContainer} />
             <ProtectedRoute path="/pets" component={PetsContainer} />
+            <ProtectedRoute path="/cart" component={Cart} />
             <AuthRoute exact path='/signup' component={SignupFormContainer} />
         </Switch>
     </div>
---
+++ frontend/src/components/cart/cart.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { fetchCart, createCart } from '../../util/cart_api_util';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Cart = (props) => {
+  const match = useRouteMatch();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchCart().then((res) => {
+      dispatch(createCart(res.data));
+    });
+  }, []);
+
+  const cart = useSelector((state) => state.cart);
+
+  const cartItems = cart.map((item) => {
+    return (
+      <li>
+        Name: {item.name} - Quantity: {item.quantity}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <div>My Cart</div>
+      <div>
+        <ul>{cartItems}</ul>
+      </div>
+      <div>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
