499de6bd71a79a8d05454c24f0dd426c54c84d53
alancln <alan.ng8912@gmail.com>
2022-07-18 16:09:19 -0400

    Delete duplicate files

--- config/keys_dev 2.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://dina-dopt-main:I6RpnMmlMTKNirtq@cluster0.tqxyr.mongodb.net/?retryWrites=true&w=majority',
-  secretOrKey: 'kmea0CNAmD',
-};
--- frontend/src/actions/cart_actions 2.js
+++
@@ -1,8 +0,0 @@
-export const RECEIVE_CART = 'RECEIVE_CART';
-
-export const receiveCart = (cart) => {
-    return {
-        type: RECEIVE_CART,
-        cart
-    }
-}
