d5d56911835def1c55536344db096225e43e2bcd
Vera Ho <vh71886@gmail.com>
2022-07-18 16:56:55 -0400

    Add about assets

--- frontend/src/components/about/about.js
+++ frontend/src/components/about/about.js
@@ -4,7 +4,7 @@ const AboutUs = props => {
 
     const developers = [{
         name: "Vera Ho - Team Lead",
-        dino: "https://cdn.discordapp.com/attachments/862515957842706475/995665506495381594/triceratops_blue.png",
+        dino: "triceratops_blue.png",
         dinoalt: "blue-triceratops",
         desc1: "Hello! I am a senior electrical engineer turned software engineer based in NYC, and currently open to opportunities as a full-stack or backend engineer.",
         desc2: "In addition to engineering, I love to hike, snowboard, and dabble in photography and sewing.",
@@ -12,10 +12,10 @@ const AboutUs = props => {
         linkedin: "https://www.linkedin.com/in/veraho/",
         angellist: "https://angel.co/u/vera-ho-1", 
         portfolio: "",
-        plogo: "https://cdn.discordapp.com/attachments/862515957842706475/995683776728399922/icons8-woman-profile-64.png"
+        plogo: "icons8-woman-profile-64.png"
     }, {
         name: "Alan Ng - FrontEnd Flex",
-        dino: "https://cdn.discordapp.com/attachments/862515957842706475/995665506696695829/triceratops_orange.png",
+        dino: "triceratops_orange.png",
         dinoalt: "orange-triceratops",
         desc1: "Hi! I am a software engineer with a passion for full-stack development. I love",
         desc2: "Some hobbies include playing basketball, snowboarding, and recently - weightlifting. I also love traveling to new places and exploring new things.",
@@ -23,10 +23,10 @@ const AboutUs = props => {
         linkedin: "https://www.linkedin.com/in/alan-cln/",
         angellist: "https://angel.co/u/alan-cheuk-lun-ng",
         portfolio: "",
-        plogo: "https://cdn.discordapp.com/attachments/862515957842706475/995683776090865734/icons8-user-64_1.png"
+        plogo: "icons8-user-64.png"
     }, {
         name: "Mike Chen",
-        dino: "https://cdn.discordapp.com/attachments/862515957842706475/995665507078393876/triceratops_red.png",
+        dino: "triceratops_red.png",
         dinoalt: "red-triceratops",
         desc1: "",
         desc2: "",
@@ -34,10 +34,10 @@ const AboutUs = props => {
         linkedin: "",
         angellist: "", 
         portfolio: "",
-        plogo: "https://cdn.discordapp.com/attachments/862515957842706475/995683776090865734/icons8-user-64_1.png"
+        plogo: "icons8-user-64.png"
     }, {
         name: "Kevin Natera",
-        dino: "https://cdn.discordapp.com/attachments/862515957842706475/995665506889637908/triceratops_purple.png",
+        dino: "triceratops_purple.png",
         dinoalt: "purple-triceratops",
         desc1: "",
         desc2: "",
@@ -45,14 +45,13 @@ const AboutUs = props => {
         linkedin: "",
         angellist: "", 
         portfolio: "",
-        plogo: "https://cdn.discordapp.com/attachments/862515957842706475/995683776090865734/icons8-user-64_1.png"
+        plogo: "icons8-user-64.png"
     }];
 
     const devItems = developers.map( developer => {
         return (
             <li className="about-page-list-item">
-                <img src={developer.dino} 
-                    alt={developer.dinoalt} />
+                <img src={require(`../../assets/images/${developer.dino}`)} alt={developer.dinoalt} />
                 <div className="developer-description">
                     <h1>{developer.name}</h1>
                     <p>{developer.desc1}</p>
@@ -75,7 +74,7 @@ const AboutUs = props => {
                     </a>
 
                     <a href={developer.portfolio}>
-                        <img src={developer.plogo}
+                        <img src={require(`../../assets/images/${developer.plogo}`)}
                             alt="portfolio-logo" />
                     </a>
                 </div>
